4cea0b507565e55474ce96e1095d7e0a6a329378
Henrik Nygren <nygrenh@gmail.com>
2019-10-30 15:00:37 +0200

    Refactor points data formatter

---
+++ frontend/util/formatPointsData.tsx
@@ -0,0 +1,51 @@
+import { UserPoints_currentUser_progresses as ProgressData } from "/static/types/generated/UserPoints"
+import { groupBy, Dictionary, mapValues, flatten } from "lodash"
+
+interface GroupPoints {
+  group: string
+  n_points: number
+  max_points: number
+  progress: number
+}
+
+interface ServiceGroupPoints extends GroupPoints {
+  service: string
+}
+
+function formatPointsData({ pointsData }: { pointsData: ProgressData }) {
+  if (!pointsData.user_course_progress) {
+    return null
+  }
+
+  const courseProgressesByGroup: Dictionary<GroupPoints[]> = groupBy(
+    pointsData.user_course_progress.progress,
+    "group",
+  )
+  const courseProgressByGroup = mapValues(courseProgressesByGroup, o => o[0])
+
+  const serviceProgressesArray = flatten(
+    pointsData.user_course_service_progresses.map(
+      o =>
+        o.progress.map((o2: GroupPoints) => {
+          return {
+            service: o.service.name,
+            group: o2.group,
+            max_points: o2.max_points,
+            n_points: o2.n_points,
+            progress: o2.progress,
+          }
+        }) as ServiceGroupPoints[],
+    ),
+  )
+
+  const serviceProgressesByGroup = groupBy(serviceProgressesArray, "group")
+
+  return mapValues(courseProgressByGroup, o => {
+    return {
+      courseProgress: o,
+      service_progresses: serviceProgressesByGroup[o.group],
+    }
+  })
+}
+
+export default formatPointsData
